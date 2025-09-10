d04ace5b4c7cdf168bd103e50ea54cccf6805085
Rory Lange <rorystrattonlange@gmail.com>
2025-09-10 01:37:31 -0400

    finish downloading the dataset

--- textrecognizer/src/textrecognizer/download_data/main.py
+++ textrecognizer/src/textrecognizer/download_data/main.py
@@ -1,4 +1,15 @@
 import kagglehub as kh
+import pandas as pd
 
+# downloads it into your computers cache
 def download():
-    kh.download_dataset
+    path = kh.competition_download('digit-recognizer', path='train.csv')
+    print("Downloaded to:", path)
+    return path
+
+# downloads the dataset locally to a parquet file
+# youll need to add this to .gitignore for whatever path you choose to download it at
+def download_locally(download_path):
+    path = download()
+    df = pd.read_csv(path)
+    df.to_parquet(download_path, engine="pyarrow", compression="snappy")
