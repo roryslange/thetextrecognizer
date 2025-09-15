c8bed7687bb769d23e5c508b8416a14928270d0a
Rory Lange <rorystrattonlange@gmail.com>
2025-09-15 01:09:21 -0400

    add feed forward to neuron

--- textrecognizer/src/textrecognizer/model/neuron.py
+++ textrecognizer/src/textrecognizer/model/neuron.py
@@ -7,3 +7,12 @@ class Neuron:
     def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
+
+
+    """
+    Basically what we want to do here is (x1 * i1) + ... + (xN * iN) + b
+    where x is a weight, i is an input, and b is the bias
+    we then want to use sigmoid on it so it stays between 0 and 1
+    """
+    def feedForward(self, inputs):
+        return sigmoid(np.dot(self.weights, inputs) + self.bias)
