d974564e92ff438a30cfa9396511b8a4e64829e8
Rory Lange <rorystrattonlange@gmail.com>
2025-09-15 01:02:56 -0400

    simple changes to neuron

--- textrecognizer/src/textrecognizer/model/neuron.py
+++ textrecognizer/src/textrecognizer/model/neuron.py
@@ -2,3 +2,8 @@ import numpy as np
 
 def sigmoid(val):
     return 1 / 1 + np.exp(-val)
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
