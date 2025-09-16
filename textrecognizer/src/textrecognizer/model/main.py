2df00f4ca2be0a377da1a39baa40a8d705007a47
Rory Lange <rorystrattonlange@gmail.com>
2025-09-15 21:08:27 -0400

    remove unused scripts, initialize data from video

--- textrecognizer/src/textrecognizer/model/main.py
+++ textrecognizer/src/textrecognizer/model/main.py
@@ -1,9 +1,24 @@
 from .dataBuilder import dataframeFromCsv
+import numpy as np
 
 def train():
-    df = dataframeFromCsv()
-    trainingNumRows = (int) (len(df) * .8)
-    testingNumRows = len(df) - trainingNumRows
-    training = df[:trainingNumRows]
-    testing = df[trainingNumRows:testingNumRows:]
-    print(f'training size (80%): {trainingNumRows}\ntesting size (20%): {testingNumRows}\ntotal rows: {len(df)}')
+    data = dataframeFromCsv()
+    print(data.head())
+
+    # the following code is based on multiple sources on how to partition the data for this example
+    # I specifically got the code from this video: https://www.youtube.com/watch?v=w8yWXqWQYmU&t=691s by Samson Zhang
+    # main idea is we want to transpose data so its easier to index, and split into dev and training
+    data = np.array(data)
+    m, n = data.shape
+    np.random.shuffle(data)
+
+    data_dev = data[0:1000].T
+    Y_dev = data_dev[0]
+    X_dev = data_dev[1:n]
+
+    data_train = data[1000:m].T
+    Y_train = data_train[0]
+    X_train = data_train[1:n]
+
+
+
